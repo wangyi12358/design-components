ee189128c5f32c7d1b247ad5ef6ddb1d7f263012
Wang Yi <1721436982@qq.com>
2023-05-31 22:09:55 +0800

    feat: add card baseprops

--- packages/ui/src/components/card/card.tsx
+++ packages/ui/src/components/card/card.tsx
@@ -1,15 +1,21 @@
 import type { FC } from 'react';
 import React, { memo } from 'react';
+import { type BaseProps } from '../_base';
+import { clsxMerge } from '@light/utils';
 
-export interface CardProps {
+export interface CardProps extends BaseProps {
   children?: React.ReactNode;
   title?: React.ReactNode;
 }
 
 const Card: FC<CardProps> = memo((props) => {
-  const { children, title } = props;
+  const { children, title, className, style } = props;
+
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+    <div
+      style={style}
+      className={clsxMerge('card card-compact w-96 bg-base-100 shadow-xl', className)}
+    >
       <div className="card-body">
         {title && <h2 className="card-title">{title}</h2>}
         {children}
--- packages/ui/src/components/card/index.md
+++ packages/ui/src/components/card/index.md
@@ -4,3 +4,7 @@ order: 0
 group:
   title: 布局
 ---
+
+## 基础使用
+
+<code src="./demos/index.tsx"></code>
